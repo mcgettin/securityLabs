8932574fc6f2862ef48ee48868496916fa182133
Neil McGettigan <c12810959@mydit.ie>
2015-01-27 20:41:21 +0200

    first security lab stuff

---
+++ lab1/pyLetters.py
@@ -0,0 +1,22 @@
+
+
+fPtr=open('sampLab.txt','r')
+dict={'a':0}
+for file in fPtr:
+	for let in file:
+		if not let.isalpha(): continue
+		let=let.lower()
+		if let in dict:
+			dict[let]+=1
+		else: dict[let]=1	
+
+fPtr.close()
+fPtr=open('outLetters.txt','w')
+for key in sorted(dict):
+	fPtr.write(key)
+	fPtr.write(':')
+	fPtr.write((str)(dict[key]))
+	fPtr.write('\n')
+	
+
+fPtr.close()

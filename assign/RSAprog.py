cf4495a64cb2b2ea9b736cb780328879a31a7d3d
Neil McGettigan <c12810959@mydit.ie>
2015-02-20 11:34:53 +0000

    fixed calc of d. very effiecient now. finding 55-bit prime takes 16 seconds. 128-bit prime: forever. need better implementation

--- assign/RSAprog.py
+++ assign/RSAprog.py
@@ -1,3 +1,24 @@
+'''
+    for getting the gcd and quotiants of a and b via extended gcd method
+'''
+def extended_gcd(a, b):
+    lastremainder, remainder = abs(a), abs(b) #disregard minus signs
+    x, lastx, y, lasty = 0, 1, 1, 0 #init egcd variables
+    while remainder: #until remainder = 0
+        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+        x, lastx = lastx - quotient*x, x
+        y, lasty = lasty - quotient*y, y
+    return lastremainder, lastx * (-1 if a < 0 else 1), lasty * (-1 if b < 0 else 1)
+
+'''
+    uses the extended gcd to find the mod inverse given a number e and a mod m 
+'''
+def modinv(e, m):
+    g, x, y = extended_gcd(e, m) #gets x and y quotient and also the gcd (hopefully=1)
+    if g != 1: #something went wrong if the gcd != 1
+        raise ValueError
+    return x % m # x is e^-1 from extended gcd , so x mod m = d (what we want)
+ 
 
 def gcd(a,b):
     while b is not 0: 
@@ -28,15 +49,18 @@ def isprime(n):
 
 import random
 
+bits=55
 ### GENERATE  p,q ###
 p=0
 q=0
+
 while not isprime(p):
-    p=random.getrandbits(8)
+    p=random.getrandbits(bits)
 print("p: "+str(p))
 
 while not isprime(q):
-    q=random.getrandbits(8)
+    q=random.getrandbits(bits)
+
 print("q: "+str(q))
 
 ### Calc n= p*q ###
@@ -48,16 +72,15 @@ y=(p-1)*(q-1)
 print("y: "+str(y))
 
 ### Generate e (gcd(e,y)=1) ###
-e=random.getrandbits(8)
+e=2**bits #small values for e does not affect security of RSA
 while gcd(e,y)!=1:
     e+=1
 print("e: "+str(e))
 
-print("\nPublic Keys (n,e): {}, {}".format(n,e))
-
-
 ### Generate d (de=1 mod (y)) ###
-d=random.getrandbits(6)
-while d*e % y is not 1:
-    d+=1
-print("Private Key (d): "+str(d))
+d = modinv(e,y)
+print("d: "+str(d))
+
+print("\nPublic Key (n,e): ({}, {})".format(n,e))
+print("Private Key (n,d): ({}, {})".format(n,d))
+
